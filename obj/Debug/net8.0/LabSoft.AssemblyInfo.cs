54e3b86a1266f9cf44f2628170047b726af525d2
melissavalenciacardona <melissa.valencia3@utp.edu.co>
2024-10-29 21:55:38 -0500

    Primera actualizacion Parcial II

--- obj/Debug/net8.0/LabSoft.AssemblyInfo.cs
+++ obj/Debug/net8.0/LabSoft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2d6e015d7d401bab8832ee8be79fcf186bf5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84145d1abef97863949edc2c63b3de7bc71970bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
